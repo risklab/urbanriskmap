4723285284428e7667b54941060cf7fd8f63924a
asbarve <adityasbarve@gmail.com>
2017-10-26 15:41:18 -0400

    updates to slideshow script and overall styling to match multipage

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,13 +1,16 @@
-<div class="col-sm-4 project-item">
-  <center> <br><H6>United States</H6>
-    <br>
-    <a href="https://riskmap.us"><img src="img/project/RiskmapUS_logo.svg" style="height:50px;"></a> <hr>
-    <p style="text-align:left;">This project, supported by USAID, the Indonesian Federal Emergency Management Agency and in collaboration with the Pacific Disaster Centre, allows residents to share vital flood information using social messaging apps. Confirmed flood reports are added to a publically available map helping to inform communities and emergency agencies of the flood situation in real-time. Currently operating in three cities, Greater Jakarta, Bandung, and Surabaya, the flood map feeds the crowdsourced data to Federal Agency disaster system through a custom built REM (Risk Evaluation Metrix).</p>
-  </center>
-</div>
-<div class="col-sm-4 project-item">
-  <center><br><H6>India</h6>
-    <br> <a href="http://riskmap.in"><img src="img/project/Riskmap_logo.svg" style="height:50px;"></a> <hr>
-    <p style="text-align:left;">This project - supported by TATA Center for Technolgy and Design at MIT - is focused on urban hazzards vulnarablity of Indian cities. Currently in the development mode for city of Chennai, this platform will gather confirmed flood reports from social media and messaging platforms which are then added to a publically available map helping to inform communities about the flood situation in real-time. This platform will feed the crowd-sourced data to Emergency Managment Cell of the Chennai Municipal corporation through a custom built REM (Risk Evaluation Metrix).</p>
-  </center>
-</div>
+function showDivs(e) {
+  var s,l = document.getElementsByClassName("mySlides");
+  console.log(l.length);
+  for (s = 0; s < l.length;s++) {
+  l[s].style.display = "none",
+  e > l.length && (slideIndex = 1),
+  e < 1 && (slideIndex = l.length);
+  l[slideIndex - 1].style.display = "block";
+  }
+}
+
+function plusDivs(e) {
+  showDivs(slideIndex += e);
+}
+var slideIndex = 1;
+showDivs(slideIndex);
