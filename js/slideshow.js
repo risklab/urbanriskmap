df317418dbab7780eae515197f18301198213201
asbarve <adityasbarve@gmail.com>
2017-10-26 14:50:13 -0400

    created saparated slideshow script

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,16 +1,16 @@
 function showDivs(e) {
-  var s, l = document.getElementsByClassName("mySlides");
-  for (console.log(l.length), s = 0; s < l.length; s++) l[s].style.display = "none", e > l.length && (slideIndex = 1), e < 1 && (slideIndex = l.length);
-  l[slideIndex - 1].style.display = "block"
+  var s,l = document.getElementsByClassName("mySlides");
+  console.log(l.length);
+  for (s = 0; s < l.length;s++) {
+  l[s].style.display = "none",
+  e > l.length && (slideIndex = 1),
+  e < 1 && (slideIndex = l.length);
+  l[slideIndex - 1].style.display = "block";
+  }
 }
 
 function plusDivs(e) {
-  showDivs(slideIndex += e)
+  showDivs(slideIndex += e);
 }
 var slideIndex = 1;
 showDivs(slideIndex);
-
-document.getElementById("Projectdropdown").onclick = function pagelinks(){
-  $('#projectsLists').show();
-  console.log("i ran");
-};
